887eb0d0e5a6923ca5dffded94082713bf30bedf
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-13 18:27:32 +0530

    [Anjaneya]Added:Reading single digit value using user input

---
+++ Selection_Ifelseif/ReadingSingleDigitNum.js
@@ -0,0 +1,26 @@
+//Taking user input here
+let readline = require('readline-sync');
+let number = readline.question("Enter number here");
+if (number == 0)
+console.log("You have entered zero");
+else if (number == 1)
+console.log("You have entered one");
+else if (number == 2)
+console.log("You have entered two");
+else if (number == 3)
+console.log("You have entered three");
+else if (number == 4)
+console.log("You have entered four");
+else if (number == 5)
+console.log("You have entered five");
+else if (number == 6)
+console.log("You have entered six");
+else if (number == 7)
+console.log("You have entered seven");
+else if (number == 8)
+console.log("You have entered eight");
+else if (number == 9)
+console.log("You have entered nine");
+else
+console.log("U have entered invalid number");
+
